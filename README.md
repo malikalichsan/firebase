83362b66b8bf0d558bc307412990673e488b39ce
Malik Al Ichsan <malik.al.ichsan@gmail.com>
2016-11-05 18:10:15 +0700

    Hapus Data

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Firebase
+
+ Demo [disini](https://todolist-8a85c.firebaseapp.com/)
--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
+    /*
+    | Simpan Data
+    */
     $('#btn_simpan').click(function() {
-        var d = new Date();
-        var n = d.getTime();
 
         // mendeklarasikan database firebase sekaligus childnya
         var database = firebase.database().ref().child('todo');
 
         database.push({
-            id_list: n,
             judul: $('#todolist').val(),
             description: $('#description').val()
         });
@@ -17,23 +17,37 @@ $(document).ready(function() {
         $('#message').html($('#berhasil_message').html());
     });
 
-    // var json = [];
-    // var db = firebase.database().ref().child('todo');
-    // db.on('value', function(data) {
-    //     json.push(data.val());
-    //     console.log(json);
-    //     // for (var i = 0; i < json.length; ++i) {
-    //     //     $('#get_list').append('<div class=\"col-md-3 list\"><div class=\"panel panel-default\"><div class=\"panel-heading\"><div class=\"row\"><div class=\"col-md-8\">'+json[i].judul+'</div><div class=\"col-md-4\"><button type=\"button\" class=\"close close_todo\" data-id=\"'+json[i].id_list+'\"><span>&times;</span></button></div></div></div><div class=\"panel-body\">'+json[i].description+'</div></div></div>');
-    //     // }
-    // });
-
+    /*
+    | Tampil Data
+    */
     var db = firebase.database().ref().child('todo');
     db.on('value', function(data) {
         $('#get_list').empty();
         data.forEach(function(data) {
-            // console.log(data.val());
-            $('#get_list').append('<div class=\"col-md-3 list\"><div class=\"panel panel-default\"><div class=\"panel-heading\"><div class=\"row\"><div class=\"col-md-8\">'+data.val().judul+'</div><div class=\"col-md-4\"><button type=\"button\" class=\"close close_todo\" data-id=\"'+data.val().id_list+'\"><span>&times;</span></button></div></div></div><div class=\"panel-body\">'+data.val().description+'</div></div></div>');
+            $('#get_list').append(
+                '<div class=\"col-md-3 list\">' +
+                    '<div class=\"panel panel-default\">' +
+                        '<div class=\"panel-heading\">' +
+                            '<div class=\"row\">' +
+                                '<div class=\"col-md-8\">'+data.val().judul+'</div>' +
+                                '<div class=\"col-md-4\">' +
+                                    '<button type=\"button\" class=\"close close_todo\" data-key=\"'+data.key+'\"><span>&times;</span></button></div>' +
+                            '</div>' +
+                        '</div>' +
+                        '<div class=\"panel-body\">'+data.val().description+'</div>' +
+                    '</div>' +
+                '</div>'
+            );
         });
+    });
+
+    $(document).on('click', '.close_todo', function() {
+        var key = $(this).data('key');
+        if (confirm("Yakin mau dihapus?")) {
+            db.child(key).remove();
+            $('#message').html($('#hapus_message').html());
+        }
+        return false;
     });
 
 });
